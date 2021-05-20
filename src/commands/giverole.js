f7a1ef634c525eed06a08b89fd1f86b98afffd54
Lucas Matheus <lucasmatheus2021@gmail.com>
2021-05-19 21:14:05 -0300

    added new command giverole and added procfile for heroku production
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

---
+++ src/commands/giverole.js
@@ -0,0 +1,34 @@
+module.exports = {
+    name: 'giverole',
+	description: 'Add a role to an user on Server',
+    aliases: ['teste', 'testeeee'],
+	expectedArgs: '<User @mention> <Role name>',
+	permisions: 'ADMINISTRATOR',
+    async execute(client, message, args) {
+		const userMention = message.mentions.users.first();
+		if (!userMention) {
+			message.reply('Please specify someone to give a role to');
+			return;
+		}
+		const mention = args.shift();
+		const roleName = args.join(' ');
+		const { guild } = message;
+		
+		const role = guild.roles.cache.find((role) => {
+			return role.name == roleName;
+		});
+		
+		if (!role) {
+			message.reply(`There is no role with the name: ${roleName}`);
+			return;
+		}
+
+		const memberMentioned = guild.members.cache.get(userMention.id);
+		if (memberMentioned.roles.add(role)) {
+			message.reply(`Congrats!🎉🎉\nThe user${mention} is now a ${roleName}`);
+		} else {
+			message.reply(`this user already have the role ${roleName}`);
+		}
+
+	}
+}
--- src/commands/teste.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    name: 'teste',
-    aliases: ['teste', 'testeeee'],
-    async execute(client, message, args) {
-		message.channel.send('ok');
-	}
-}
