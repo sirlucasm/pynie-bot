19cae57d7c74cc98486f3bb47b2cb59f9d4b4f8a
Lucas Matheus <lucasmatheus2021@gmail.com>
2021-05-21 15:15:30 -0300

    fix procfile
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
 require('dotenv').config();
-const express = require('express');
-const app = express()
-const PORT = process.env.PORT || 3000;
 
 const PynieBot = require('./src/index');
 
-app.listen(PORT, () => {
-	console.log(`\nPynie Bot runnign at http://localhost:${PORT}\n`);
-	PynieBot.run();
-});
+PynieBot.run()
