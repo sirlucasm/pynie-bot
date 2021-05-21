4da0990aec33d3e2ea6cd94b0f08bc27a78ef3c1
Lucas Matheus <lucasmatheus2021@gmail.com>
2021-05-21 13:43:35 -0300

    added bot server setup
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
+const express = require('express');
+const app = express()
+const PORT = process.env.PORT || 3000;
 
 const PynieBot = require('./src/index');
 
-PynieBot.run()
+app.listen(PORT, () => {
+	console.log(`\nPynie Bot runnign at http://localhost:${PORT}\n`);
+	PynieBot.run();
+});
